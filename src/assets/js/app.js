4c70f88d189bf75fb1fff771cfe2b7556a9e2c45
ValouZ <debray.va@gmail.com>
2021-05-04 16:07:56 +0200

    ajout bouton deco

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,21 +1,34 @@
-// permet de récupérer le dernier morceau de l'url
-let lastIndexofURN = document.location.href.lastIndexOf("/");
-let slicedURN = document.location.href.slice(lastIndexofURN);
+redirection();
 
-if (
-  !(
-    slicedURN === "/" ||
-    slicedURN === "/index.html" ||
-    slicedURN === "/sign-up.html"
-  )
-) {
-  // Si on n'est pas sur la page d'accueil
-  if (!localStorage.getItem("token")) {
-    document.location.href = "/";
+
+let dcButton = document.querySelector("#app-dc");
+
+dcButton.addEventListener("click", dc);
+
+function redirection() {
+  // permet de récupérer le dernier morceau de l'url
+  let lastIndexofURN = document.location.href.lastIndexOf("/");
+  let slicedURN = document.location.href.slice(lastIndexofURN);
+
+  if (
+    !(
+      slicedURN === "/" ||
+      slicedURN === "/index.html" ||
+      slicedURN === "/sign-up.html"
+    )
+  ) {
+    // Si on n'est pas sur la page d'accueil
+    if (!localStorage.getItem("token")) {
+      document.location.href = "/";
+    }
+  } else {
+    // Si on est sur les autres pages
+    if (localStorage.getItem("token")) {
+      document.location.href = "/user.html";
+    }
   }
-} else {
-  // Si on est sur les autres pages
-  if (localStorage.getItem("token")) {
-    document.location.href = "/user.html";
-  }
+}
+
+function dc() {
+  console.log(dcButton);
 }
